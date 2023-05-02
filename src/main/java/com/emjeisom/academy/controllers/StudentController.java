6a0bd6df549c9635d3a79f33aa16d5a13392078b
emjeisomDev <emjeisom.dev@gmail.com>
2023-05-02 15:38:56 -0300

    Configured methods update and delete student

--- src/main/java/com/emjeisom/academy/controllers/StudentController.java
+++ src/main/java/com/emjeisom/academy/controllers/StudentController.java
@@ -3,6 +3,7 @@ package com.emjeisom.academy.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -31,11 +32,35 @@ public class StudentController {
 		return mv;
 	}
 	
+	@GetMapping("/update/{id}")
+	public ModelAndView update(@PathVariable("id") Integer id) {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("student/update");
+		Student student = studentRepository.getReferenceById(id);
+		mv.addObject("student", student);
+		return mv;
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteStudent(@PathVariable("id") Integer id) {
+		studentRepository.deleteById(id);
+		String url = "redirect:/list-students";
+		return url;
+	}
+	
 	@PostMapping("/insert-students")
 	public ModelAndView postStudents(Student student) {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("redirect:/list-students");
 		studentRepository.save(student);
+		return mv;
+	}
+	
+	@PostMapping("/update")
+	public ModelAndView update(Student student) {
+		ModelAndView mv = new ModelAndView();
+		studentRepository.save(student);
+		mv.setViewName("redirect:/list-students");
 		return mv;
 	}
 	
